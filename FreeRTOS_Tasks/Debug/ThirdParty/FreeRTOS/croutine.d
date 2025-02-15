4ccb824f41b90bd46afb26dc877a3056c113a2c3
Miguel Castorena <miguel.angel.castorena@gmail.com>
2025-02-15 13:09:52 -0800

    Init commit. Initialize FreeRTOS tasks demo application

---
+++ FreeRTOS_Tasks/Debug/ThirdParty/FreeRTOS/croutine.d
@@ -0,0 +1,21 @@
+ThirdParty/FreeRTOS/croutine.o: ../ThirdParty/FreeRTOS/croutine.c \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/task.h \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/list.h \
+ /home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/croutine.h
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/portable.h:
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/task.h:
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/list.h:
+/home/engineering/STM32CubeIDE/workspace_1.16.1/FreeRTOS_Tasks/ThirdParty/FreeRTOS/include/croutine.h:
